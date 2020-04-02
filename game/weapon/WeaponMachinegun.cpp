b819a1fd1596193146f3e8c07a7e48abc077f3f1
chris <cla24@njit.edu>
2020-04-02 07:20:43 -0400

    changed weapons/timestamp

--- game/weapon/WeaponMachinegun.cpp
+++ game/weapon/WeaponMachinegun.cpp
@@ -228,7 +228,7 @@ stateResult_t rvWeaponMachinegun::State_Fire ( const stateParms_t& parms ) {
 		case STAGE_INIT:
 			if ( wsfl.zoom ) {
 				nextAttackTime = gameLocal.time + (altFireRate * owner->PowerUpModifier ( PMOD_FIRERATE ));
-				Attack ( true, 1, spreadZoom, 0, 1.0f );
+				Attack ( true, 20, spreadZoom, 0, 1.0f );
 				fireHeld = true;
 			} else {
 				nextAttackTime = gameLocal.time + (fireRate * owner->PowerUpModifier ( PMOD_FIRERATE ));
